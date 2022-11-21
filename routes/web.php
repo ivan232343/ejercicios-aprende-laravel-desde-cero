19968ac713a4dc47bf43f38d3208269365c9c31d
ivan232343 <ivanchiroque2@gmail.com>
2022-11-21 16:45:31 -0500

    solucion2Error

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,118 @@ Route::get('/', function () {
 
 // Ejercicio 1
 
-Route::get('/ejercicio1', function () {
-    return "GET OK";
+// Route::get('/ejercicio1', function () {
+//     return "GET OK";
+// });
+
+// Route::post('/ejercicio1', function () {
+//     return "POST OK";
+// });
+
+// Ejercicio2
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json(
+        [
+            "name" => $request->get('name'),
+            "description" => $request->get('description'),
+            "price" => $request->get('price')
+        ]
+    );
 });
 
-Route::post('/ejercicio1', function () {
-    return "POST OK";
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request->get('price') < 0) {
+        return Response::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
+    }
+    return Response::json(
+        [
+            "name" => $request->get('name'),
+            "description" => $request->get('description'),
+            "price" => $request->get('price')
+        ]
+    );
+});
+
+// Ejercicio2
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json(
+        [
+            "name" => $request->get('name'),
+            "description" => $request->get('description'),
+            "price" => $request->get('price')
+        ]
+    );
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request->get('price') < 0) {
+        return Response::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
+    }
+    return Response::json(
+        [
+            "name" => $request->get('name'),
+            "description" => $request->get('description'),
+            "price" => $request->get('price')
+        ]
+    );
+});
+
+// Route::post('/ejercicio2/c', function (Request $request) {
+//     $name = $request->get('name');
+//     $description = $request->get('description');
+//     switch ($request->query('discount')) {
+//         case 'SAVE5':
+//             $discount = 5;
+//             break;
+//         case 'SAVE10':
+//             $discount = 10;
+//             break;
+//         case 'SAVE15':
+//             $discount = 15;
+//             break;
+//         default:
+//             $discount = 0;
+//             break;
+//     }
+//     $price = (100 - $discount) / 100 * $request->get('price');
+//     return Response::json([
+//         'name' => $name,
+//         'description' => $description,
+//         'price' => $price,
+//         'discount' => $discount,
+//     ]);
+// });
+
+Route::post('/ejercicio2/c', function (Request $request) {
+
+    // Una forma simple para sacar el discount, con if-else
+    $discount = 0;
+    if ($request->query('discount') == "SAVE5") {
+        $discount = 5;
+    } else if ($request->query('discount') == "SAVE10") {
+        $discount = 10;
+    } else if ($request->query('discount') == "SAVE15") {
+        $discount = 15;
+    }
+
+    // Otra forma de sacar el discount, con el match de PHP 8
+    // $discount = match ($request->query('discount')) {
+    //     'SAVE5' => 5,
+    //     'SAVE10' => 10,
+    //     'SAVE15' => 15,
+    //     default => 0,
+    // };
+
+    // Calcular precio nuevo
+    $price = (100 - $discount) / 100 * $request->get('price');
+
+    // Devolver respuesta
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $price,
+        'discount' => $discount,
+    ]);
 });
