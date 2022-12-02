67a75a61144dc8d429257367a3deb8eea86dd475
ivan232343 <ivanchiroque2@gmail.com>
2022-12-02 07:35:17 -0500

    pre validaciones

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,19 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+Route::post('/ejercicio3', function (Request $request) {
+    $request->validate([
+        'name' => 'required|max:64',
+        'description' => 'required|max:512',
+        'price' => 'required|min:0',
+        'has_battery' => 'boolean',
+        'battery_duration' => 'exclude_if:has_battery,false|min:0|integer|nullable',
+        'colors' => 'required_array_keys',
+        'dimensions.width' => 'min:0',
+        'dimensions.height' => 'min:0',
+        'dimensions.length' => 'min:0',
+        'accessories.name' => 'string',
+        'accessories.price' => 'numeric|min:0'
+
+    ]);
+});
